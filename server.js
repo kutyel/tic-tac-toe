40b51c2f7d76e796ddd6132041c43ad7eb7c4ce0
kutyel <flaviocorpa@gmail.com>
2018-05-13 13:50:33 +0200

    Hide compiled files

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const path = require('path');
-const express = require('express');
-const app = express();
+const path = require('path')
+const express = require('express')
+const app = express()
 // Pass the express app to our http module, which we'll also pass to socket.io.
-const http = require('http').Server(app);
-const startSocketIOServer = require('./src/Server.bs.js').startSocketIOServer;
+const http = require('http').Server(app)
+const startSocketIOServer = require('./src/Server.js').startSocketIOServer
 
-startSocketIOServer(http);
+startSocketIOServer(http)
 
 // Turn this server into a static server (so it can requests all the JS files one by one).
 app.use(express.static(path.join(__dirname)))
 
 // Serve the main html page.
-app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, 'src', 'index.html'));
-});
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'src', 'index.html'))
+})
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
+http.listen(3000, function() {
+  console.log('listening on *:3000')
+})
